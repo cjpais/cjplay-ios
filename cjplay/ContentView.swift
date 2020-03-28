799d7b7687f7e5c52679aa7e496577bd8eeb68be
CJ Pais <cjpais@ucsd.edu>
2020-03-28 11:39:21 -0700

    Initial Commit

---
+++ cjplay/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  cjplay
+//
+//  Created by CJ Pais on 3/28/20.
+//  Copyright © 2020 CJ Pais. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
